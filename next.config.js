c3e0be21d6d4eff7ef245a1feb7684d7ce516a37
Ritik <Ritikyadav10888@gmail.com>
2026-02-05 13:18:07 +0530

    Fix: Exclude build manifests from PWA precaching to solve 404 error

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require("next-pwa")({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
+    buildExcludes: [/middleware-manifest\.json$/, /_next\/app-build-manifest\.json$/, /_next\/static\/chunks\/pages\/_error\.js$/],
 });
 
 const nextConfig = {};
